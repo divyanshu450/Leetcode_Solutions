90bf99172d77cae38396ef524d1272e060b4e97f
Ghost <40882925+divyanshu450@users.noreply.github.com>
2026-02-06 20:06:28 -0800

    Time: 0 ms (100%), Space: 53.1 MB (33.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,17 +2,17 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int left = 0; int right = nums.length - 1;
 
-        while(left < right){
-            int mid = ( left+right ) / 2;//get mid
+        while(left < right){// T
+            int mid = ( left+right ) / 2;//get mid | 4, 2, 1
 
-            if(mid % 2 == 1){//not even
-                mid--;
+            if(mid % 2 == 1){//not even | F, F, T
+                mid--;//T-> 0
             }
-            if(nums[mid] == nums[mid + 1]){//
-                left = mid + 2;
+            if(nums[mid] == nums[mid + 1]){// 3 == 4-> F(broken pair), 2 == 3->F, 1 == 1->T(pair not broken)
+                left = mid + 2;// 0 + 2 = 2--> left == right and loop ends
             }
             else{
-                right = mid;
+                right = mid;//4, 2
             }
         }
         return nums[left];
