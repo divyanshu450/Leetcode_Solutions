d242bd4e8164391aa9cd1707cb844744d9e25fbd
Ghost <40882925+divyanshu450@users.noreply.github.com>
2026-02-15 11:49:07 +0530

    Time: 137 ms (11.99%), Space: 214.9 MB (75.87%) - LeetHub

---
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqueFreq(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // step 1: count freq first
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);//maps like number:freq
+        }
+        //step 2: count how many times each freq appears
+        Map<Integer, Integer> hm = new HashMap<>();// maps like freq: freq count eg: 30: 2
+        for(int count : map.values()){
+            hm.put(count, hm.getOrDefault(count, 0)+1);
+        }
+
+        //step 3: return first unique freq
+        for(int n : nums){
+            if(hm.get(map.get(n)) == 1){//get this number's freq, check how many number's have that freq, == 1 meaning it is unique;
+                return n;
+            }
+        }
+        return -1;
+    }
+}
